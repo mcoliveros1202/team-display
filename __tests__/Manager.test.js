e86a544d991ee5147e3b1754193f227b07053394
moliveros <mcoliveros1202@gmail.com>
2022-07-04 13:06:58 -0500

    created and passed tests for manager methods

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,8 +3,22 @@ const Manager = require('../lib/Manager');
 test('creates a Manager object', () => {
     const manager = new Manager('Dave');
 
+    expect(manager.job).toBe('Manager');
     expect(manager.name).toBe('Dave');
     expect(manager.id).toEqual(expect.any(Number));
     expect(manager.email).toBe('email@email.com');
     expect(manager.office).toEqual(expect.any(Number));
+
+});
+
+test("gets manager's role as an object", () => {
+    const manager = new Manager('Dave');
+
+    expect(manager.getRole()).toHaveProperty('role', 'Manager');
+});
+
+test("get manager's office number as an object", () => {
+    const manager = new Manager('Dave');
+
+    expect(manager.getOffice()).toHaveProperty('room', 1)
 });
